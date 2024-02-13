7a1ca9ca9c67c70fa7d176e02bbd63d5530776cf
Stasiškis Mantas <mantas.stasiskis@mif.stud.vu.lt>
2024-02-13 12:10:19 +0200

    Created shooting projectiles

--- src/index.js
+++ src/index.js
@@ -19,9 +19,57 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    drawProjectile() {
+        context.beginPath() 
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        context.fillStyle = this.color
+        context.fill()
+    }
+
+    update() {
+        this.drawProjectile()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
 const x = canvas.width / 2
 const y = canvas.height /2
 
 const player = new Player(x, y, 30, 'pink')
-player.drawPlayer()
-console.log(Player)
+
+const projectiles = []
+
+
+function animate() {
+    requestAnimationFrame(animate)
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    player.drawPlayer()
+    projectiles.forEach(projectile => {
+        projectile.update()
+    })
+}
+
+addEventListener('click', (event) => {
+    const angle = Math.atan2(
+        event.clientY - canvas.height / 2,
+        event.clientX - canvas.width / 2
+    )
+
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
+    
+})
+
+animate()
