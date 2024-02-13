c7f4e6486880185ef1b6e598a3af182b25e98a7c
Stasiškis Mantas <mantas.stasiskis@mif.stud.vu.lt>
2024-02-13 11:27:11 +0200

    Creating a player

--- src/index.js
+++ src/index.js
@@ -4,4 +4,24 @@ const context = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-console.log(context)
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+    drawPlayer() {
+        context.beginPath() 
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        context.fillStyle = this.color
+        context.fill()
+    }
+}
+
+const x = canvas.width / 2
+const y = canvas.height /2
+
+const player = new Player(x, y, 30, 'pink')
+player.drawPlayer()
+console.log(Player)
