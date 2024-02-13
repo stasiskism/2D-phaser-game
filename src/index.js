7189b695904e9b4c2632f72b617c43390f8527c5
Stasiškis Mantas <mantas.stasiskis@mif.stud.vu.lt>
2024-02-13 12:32:44 +0200

    Creating enemies

--- src/index.js
+++ src/index.js
@@ -40,12 +40,65 @@ class Projectile {
         this.y = this.y + this.velocity.y
     }
 }
+
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    drawProjectile() {
+        context.beginPath() 
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        context.fillStyle = this.color
+        context.fill()
+    }
+
+    update() {
+        this.drawProjectile()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
 const x = canvas.width / 2
 const y = canvas.height /2
 
 const player = new Player(x, y, 30, 'pink')
 
 const projectiles = []
+
+const enemies = []
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = 30
+
+        let x
+        let y
+
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+        const color = 'green'
+        const angle = Math.atan2(
+            canvas.height / 2 - y,
+            canvas.width / 2 - x
+        )
+    
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        enemies.push(new Enemy(x, y, radius, color, velocity));
+    }, 1000)
+}
 
 
 function animate() {
@@ -54,6 +107,9 @@ function animate() {
     player.drawPlayer()
     projectiles.forEach(projectile => {
         projectile.update()
+    })
+    enemies.forEach(enemies => {
+        enemies.update()
     })
 }
 
@@ -73,3 +129,4 @@ addEventListener('click', (event) => {
 })
 
 animate()
+spawnEnemies()
