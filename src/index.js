9345769a5c1f5b7496f4f2b28c274e24925f45e5
Stasiškis Mantas <mantas.stasiskis@mif.stud.vu.lt>
2024-02-14 19:20:06 +0200

    Added particle explosion on hit

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ class Enemy {
         this.color = color
         this.velocity = velocity
     }
-    drawProjectile() {
+    drawEnemy() {
         context.beginPath() 
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         context.fillStyle = this.color
@@ -57,9 +57,40 @@ class Enemy {
     }
 
     update() {
-        this.drawProjectile()
+        this.drawEnemy()
         this.x = this.x + this.velocity.x
         this.y = this.y + this.velocity.y
+    }
+}
+
+const friction = 0.99
+class Particle {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+        this.alpha = 1
+    }
+    drawParticle() {
+        context.save()
+        // fades away hit particles
+        context.globalAlpha = this.alpha
+        context.beginPath() 
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        context.fillStyle = this.color
+        context.fill()
+        context.restore()
+    }
+
+    update() {
+        this.drawParticle()
+        this.velocity.x *= friction
+        this.velocity.y *= friction
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+        this.alpha -= 0.01
     }
 }
 
@@ -72,13 +103,15 @@ const projectiles = []
 
 const enemies = []
 
+const particles = []
+
 function spawnEnemies() {
     setInterval(() => {
-        const radius = 30
+        const radius = Math.random() * (30 - 6) + 6
 
         let x
         let y
-
+        // for spawning from different directions and on the borders of the page
         if (Math.random() < 0.5) {
             x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
             y = Math.random() * canvas.height
@@ -86,12 +119,13 @@ function spawnEnemies() {
             x = Math.random() * canvas.width
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
-        const color = 'green'
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
+        // an angle for the velocity
         const angle = Math.atan2(
             canvas.height / 2 - y,
             canvas.width / 2 - x
         )
-    
+        // enemy direction
         const velocity = {
             x: Math.cos(angle),
             y: Math.sin(angle)
@@ -100,13 +134,36 @@ function spawnEnemies() {
     }, 1000)
 }
 
+//AnimationID for stopping the game when player dies
 let animationID
 function animate() {
     animationID = requestAnimationFrame(animate)
-    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.fillStyle = 'white'
+    context.fillRect(0, 0, canvas.width, canvas.height)
     player.drawPlayer()
-    projectiles.forEach(projectile => {
+
+    // particle dissapearence
+    particles.forEach((particle, particleIndex) => {
+        if (particle.alpha <= 0) {
+            particles.splice(particleIndex, 1)
+        } else {
+        particle.update()
+        }
+    })
+
+    projectiles.forEach((projectile, projectileIndex) => {
         projectile.update()
+
+        // deletion of projectiles when goes out of page border
+        if (projectile.x + projectile.radius < 0 ||
+            projectile.x - projectile.radius > canvas.width ||
+            projectile.y + projectile.radius < 0 ||
+            projectile.y - projectile.radius > canvas.height) {
+            setTimeout(() => {
+            projectiles.splice(projectileIndex, 1) 
+            }, 0)
+        }
+        
     })
     enemies.forEach((enemy, enemyIndex) => {
         enemy.update()
@@ -121,6 +178,11 @@ function animate() {
            const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
            //enemy and projectile collision
            if (distance - enemy.radius - projectile.radius < 1) {
+            for (let i = 0; i < 8; i++) {
+                particles.push(new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color,
+                    {x: (Math.random() - 0.5) * (Math.random() * 5),
+                    y: (Math.random() - 0.5) * (Math.random() * 5)}))
+            }
             setTimeout(() => {
                 enemies.splice(enemyIndex, 1)
                 projectiles.splice(projectileIndex, 1)
@@ -129,6 +191,7 @@ function animate() {
            }
         });
     })
+    
 }
 
 addEventListener('click', (event) => {
@@ -138,8 +201,8 @@ addEventListener('click', (event) => {
     )
 
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5
     }
 
     projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
