2df51152efa88c1b4fa49fda3ddfd54762f1b568
Lukoševičius Tautvydas <tautvydas.lukosevicius@mif.stud.vu.lt>
2024-02-21 17:04:56 +0200

    Add new file

---
+++ src/main.js
@@ -0,0 +1,20 @@
+/* global Phaser */
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    backgroundColor: 0x5F6e7a,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+}
+
+const game = new Phaser.Game(config)
