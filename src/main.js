867c82d4a42534a96741501a46a35f42501abb20
Stasiškis Mantas <mantas.stasiskis@mif.stud.vu.lt>
2024-02-24 12:16:38 +0200

    Added player shooting

---
+++ src/main.js
@@ -0,0 +1,30 @@
+/* global Phaser */
+
+import Scene1 from './scene1.js'
+import MainMenu from './MainMenu.js'
+import GameScene from './GameScene.js'
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    backgroundColor: 0x5F6e7a,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+}
+
+const game = new Phaser.Game(config)
+
+game.scene.add('scene1', Scene1)
+game.scene.add('mainMenu', MainMenu)
+game.scene.add('gameScene', GameScene)
+
+game.scene.start('scene1')
---
+++ src/scene1.js
@@ -0,0 +1,33 @@
+/* global Phaser */
+
+class Scene1 extends Phaser.Scene {
+    constructor() {
+        super({ key: 'scene1'});
+    }
+
+    init (data) {
+        this.cameras.main.setBackgroundColor('#ffffff')
+    }
+
+    preload () {
+        console.log('scene1 scene')
+        this.load.image('vaizdas', 'assets/INTRO.png')
+
+    }
+
+    create (data) {
+      const centerX = this.cameras.main.width / 2;
+      const centerY = this.cameras.main.height / 2;
+
+      this.vaizdasImage = this.add.sprite(centerX, centerY, 'vaizdas');
+
+    }
+
+    update (time, delta) {
+        if (time > 5000){
+        this.scene.switch('mainMenu')
+        }
+    }
+}
+
+export default Scene1
