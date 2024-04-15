b0e95c279b532246a237bafbe3e4f0e775cff7c8
Lukoševičius Tautvydas <tautvydas.lukosevicius@mif.stud.vu.lt>
2024-04-15 19:56:50 +0000

    Update src/login.js

--- src/login.js
+++ src/login.js
@@ -3,7 +3,6 @@ class Login extends Phaser.Scene {
         super({ key: 'login'});
     }
     init() {
-        //this.cameras.main.setBackgroundColor('#ffffff')
     }
     preload() {
 
@@ -13,18 +12,37 @@ class Login extends Phaser.Scene {
         const centerY = this.cameras.main.height / 2;
         this.add.sprite(centerX, centerY, 'menu');
         const login = this.add.dom(centerX, centerY).createFromHTML(`
-            <form id="login">
-                <div>
-                    <input type="text" id="uname" placeholder="Username" name="username" class="forminput" required><br>
-                </div>
-                <div>
-                    <input type="password" id="pswd" placeholder="Password" required><br>
-                </div>
-                <div>
-                    <input type="submit" value="Login">
-                </div>
-            </form>
-            <p style="color:white">Not a member? <a href="#" id="register">Sign up now</a></p>
+        <style>
+    #login {
+        background-color: rgba(255, 255, 255, 0.5);
+        padding: 20px;
+        border-radius: 5px;
+        box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+        text-align: center;
+    }
+    #login input[type="text"],
+    #login input[type="password"] {
+        width: 80%;
+        padding: 10px;
+        margin: 10px auto;
+        border-radius: 5px;
+        border: 1px solid #ccc;
+        display: block;
+    }
+</style>
+
+<form id="login">
+    <div>
+        <input type="text" id="uname" placeholder="Username" name="username" class="forminput" required>
+    </div>
+    <div>
+        <input type="password" id="pswd" placeholder="Password" required>
+    </div>
+    <div>
+        <input type="submit" value="Login" style="width: 80%; padding: 10px; border-radius: 5px; border: none; color: white; background-color: #5C6BC0;">
+    </div>
+</form>
+<p style="color:white">Not a member? <a href="#" id="register">Sign up now</a></p>
         `);
 
         const register = login.getChildByID('register');
@@ -48,9 +66,8 @@ class Login extends Phaser.Scene {
         })
 
     }
-        
 
-    
+
     update() {
 
     }
@@ -60,37 +77,27 @@ class Login extends Phaser.Scene {
     }
 
     sendData(username, password) {
-        const data = {username, password}
-        socket.emit('login', data)
-        socket.on('loginResponse', (response) => {
-            if (response.success) {
-                alert('Login successful');
-                this.scene.start('Multiplayer');
-            } else {
-                alert('Login failed');
-            }
-        });
-        // fetch('/login', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({username, password})
-        // })
-        // .then(response => {
-        //     if (response.ok) {
-        //         alert('Login successful')
-        //         this.scene.start('Multiplayer')
-        //     } else {
-        //         alert('Login failed')
-        //     }
-        // })
-        // .catch(error => {
-        //     console.log('Error:', error)
-        //     alert('An error occured')
-        // })
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, password})
+        })
+            .then(response => {
+                if (response.ok) {
+                    alert('Login successful')
+                    this.scene.start('scene1')
+                } else {
+                    alert('Login failed')
+                }
+            })
+            .catch(error => {
+                console.log('Error:', error)
+                alert('An error occured')
+            })
     }
 
 }
 
-export default Login
+export default Login;
