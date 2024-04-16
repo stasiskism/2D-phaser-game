13e0baf14d297be35a824aea5a8def93061baae4
Lukoševičius Tautvydas <tautvydas.lukosevicius@mif.stud.vu.lt>
2024-04-16 09:34:19 +0000

    Update src/Singleplayer.js

--- src/Singleplayer.js
+++ src/Singleplayer.js
@@ -162,6 +162,7 @@ this.anims.create({
     this.bullets = []
     this.enemies = []
     //for everything else to load we need to delay the spawning of enemies
+    
     this.time.delayedCall(500, this.spawnEnemies, [], this);
     this.input.on('pointerdown', this.fireBullet, this);
 
